9d5d3d707975c65ca1d35f4f75cddd7a4e34d48a
Nolan Neeley <nolanneeley@gmail.com>
2018-11-20 16:03:09 -0600

    got 3rd passing test for free on previous code. this time wrote 4th passing test

--- ruby/bob/bob.rb
+++ ruby/bob/bob.rb
@@ -3,6 +3,8 @@ class Bob
   def self.hey(something)
     if something == something.upcase
       "Whoa, chill out!"
+    elsif something.split("").last == "?"
+      "Sure."
     else
       "Whatever."
     end
--- ruby/bob/bob_test.rb
+++ ruby/bob/bob_test.rb
@@ -16,13 +16,13 @@ class BobTest < Minitest::Test
   end
 
   def test_shouting_gibberish
-    skip
+    # skip
     remark = "FCECDFCAAB"
     assert_equal "Whoa, chill out!", Bob.hey(remark), %q{Bob hears "FCECDFCAAB", and..}
   end
 
   def test_asking_a_question
-    skip
+    # skip
     remark = "Does this cryogenic chamber make me look fat?"
     assert_equal "Sure.", Bob.hey(remark), %q{Bob hears "Does this cryogenic chamber make me look fat?", and..}
   end
