6891140c31ea6d9c80930f0ad32618b68d733c3d
Nolan Neeley <nolanneeley@gmail.com>
2018-11-20 17:13:04 -0600

    updated something to message

--- ruby/bob/bob.rb
+++ ruby/bob/bob.rb
@@ -1,30 +1,31 @@
 class Bob
 
-  def self.yelling_question(something)
-    (something.split("").last == "?") && (something == something.upcase) && something.count("a-zA-Z") > 0
+
+  def self.yelling_question(message)
+    (message.split("").last == "?") && (message == message.upcase) && message.count("a-zA-Z") > 0
   end
 
-  def self.yelling(something)
-    something == something.upcase && something.count("a-zA-Z") > 0
+  def self.yelling(message)
+    message == message.upcase && message.count("a-zA-Z") > 0
   end
 
-  def self.asking_question(something)
-    something.strip.split("").last == "?"
+  def self.asking_question(message)
+    message.strip.split("").last == "?"
   end
 
-  def self.nothing_said(something)
-    something.strip.empty?
+  def self.nothing_said(message)
+    message.strip.empty?
   end
 
-  def self.hey(something)
+  def self.hey(message)
     case
-    when yelling_question(something)
+    when yelling_question(message)
       "Calm down, I know what I'm doing!"
-    when yelling(something)
+    when yelling(message)
       "Whoa, chill out!"
-    when asking_question(something)
+    when asking_question(message)
       "Sure."
-    when nothing_said(something)
+    when nothing_said(message)
       "Fine. Be that way!"
     else
       "Whatever."
