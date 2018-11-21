b7834298924a6f104cd72ffed23ddff721d6d1e9
Nolan Neeley <nolanneeley@gmail.com>
2018-11-20 22:18:49 -0600

    passed all tests

--- ruby/leap/leap.rb
+++ ruby/leap/leap.rb
@@ -1,7 +1,28 @@
 class Year
-
   def self.leap?(year)
 
-  end
+    is_leap = false
 
+    # on every year that is evenly divisible by 4
+    if year % 4 == 0
+      is_leap = true
+    end
+
+    # except every year that is evenly divisible by 100
+    if year % 100 == 0
+      is_leap = false
+    end
+
+    # unless the year is also evenly divisible by 400
+    if year % 400 == 0
+      is_leap = true
+    end
+
+    return is_leap
+
+
+
+
+
+  end
 end
