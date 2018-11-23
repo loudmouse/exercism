0d73dac743c82e26b59da56a50d7df63baa0716d
Nolan Neeley <nolanneeley@gmail.com>
2018-11-23 17:39:58 -0600

    passed all tests

--- ruby/acronym/acronym.rb
+++ ruby/acronym/acronym.rb
@@ -1,7 +1,7 @@
 class Acronym
 
   def self.abbreviate(string)
-    words = string.split(" ")
+    words = string.scan(/\w+/)
     first_letters = words.map { |letter| letter.byteslice(0)  }
     acronym = first_letters.join.upcase
     return acronym
