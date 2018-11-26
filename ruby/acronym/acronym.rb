18ebc318c23386db788e4ca45b6a9a8bf4d3eaac
Nolan Neeley <nolanneeley@gmail.com>
2018-11-26 16:22:03 -0600

    refactored the regex to use word boundary at recommendation of mentor although it seems to be more complicated this way. need to get feedback

--- ruby/acronym/acronym.rb
+++ ruby/acronym/acronym.rb
@@ -1,7 +1,7 @@
 class Acronym
 
   def self.abbreviate(string)
-    words = string.scan(/\w+/)
+    words = string.scan(/\b\w+\b/)
     first_letters = words.map { |word| word.chr  }
     acronym = first_letters.join.upcase
   end
